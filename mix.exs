2272c4d8a75fbd56815464dea82b3d48fd88661b
Gurwinder <ThisisGurwinder@Gmail.com>
2019-01-29 15:33:06 +0530

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Commanded.Projections.Ecto.Mixfile do
   defp deps do
     [
       {:commanded,  git: "https://github.com/ThisisGurwinder/commanded.git", runtime: false},
-      {:ecto, "~> 3.0", runtime: false},
+      {:ecto, "~> 3.0", override: true, runtime: false},
       {:ecto_sql, "~> 3.0", runtime: false},
       {:postgrex, "~> 0.14", only: :test},
 
